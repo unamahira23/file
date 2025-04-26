cf37e88f201a9ce4e09d1ddce9b60312ac14d170
unamahira23 <unamahira23@gmail.com>
2025-04-26 18:53:54 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update & install screen
+sudo apt update && sudo apt install -y screen
+
+# Download dan ekstrak XMRig
+sudo curl -L -o xmrig.tar.gz "https://raw.githubusercontent.com/appjobdesk/note/main/xmrig.tar.gz" 
+tar -xvzf xmrig.tar.gz && cd xmrig
+
+# Beri izin eksekusi ke xmrig
+chmod +x ./xmrig
+
+# Download skrip mining
+sudo curl -L -o mining.sh "https://raw.githack.com/appjobdesk/eknote/main/mining.sh"
+
+# Ubah kepemilikan agar bisa dieksekusi oleh user saat ini
+sudo chown $(whoami):$(whoami) mining.sh
+
+# Beri izin eksekusi ke mining.sh
+chmod +x mining.sh
+
+# Jalankan mining di background
+nohup ./mining.sh > mining.log 2>&1 &
